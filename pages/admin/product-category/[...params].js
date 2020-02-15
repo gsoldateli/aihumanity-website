939d337027adb5d9cb2cbd70108b358149bc32ca
Guilherme Soldateli <guilherm3soldateli@gmail.com>
2020-02-15 18:22:31 -0300

    Product category page customization

--- pages/admin/product-category/[...params].js
+++ pages/admin/product-category/[...params].js
@@ -1,8 +1,15 @@
 import { useState } from "react";
 import { useForm, Controller } from "react-hook-form";
-import { Form, Button, Input, TextArea } from "semantic-ui-react";
+import {
+  Form,
+  Button,
+  Input,
+  TextArea,
+  Table,
+  Dropdown
+} from "semantic-ui-react";
 import AdminLayout from "../../../components/Admin/Layout";
-import Crud from "../../../components/Admin/Crud";
+import Crud, { CrudList } from "../../../components/Admin/Crud";
 import withAuth from "../../../components/Admin/withAuth";
 
 const ProductCategoryForm = props => {
@@ -57,11 +64,39 @@ const ProductCategoryForm = props => {
   );
 };
 
+const ProductCategoryList = ({ api }) => {
+  return (
+    <CrudList
+      api={api}
+      columns={["Nome", "Descrição"]}
+      renderItem={item => {
+        return (
+          <>
+            <Table.Cell>{item.name}</Table.Cell>
+            <Table.Cell>{item.description}</Table.Cell>
+          </>
+        );
+      }}
+      extraItemActions={item => (
+        <>
+          <Dropdown.Item
+            text={`Alert ${item.id}`}
+            onClick={() => alert(item.id)}
+          />
+        </>
+      )}
+    />
+  );
+};
+
 const ProductCategoryCrud = () => {
   return (
     <AdminLayout>
       <Crud
+        title="Categorias de produto"
+        subtitle="Gerencie aqui as categorias de produtos"
         FormComponent={ProductCategoryForm}
+        ListComponent={ProductCategoryList}
         resourceEndpoint="product-category"
       />
     </AdminLayout>
